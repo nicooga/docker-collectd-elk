ea3522940317372e9788ec43535ad6f70d15e31a
Dino Korah <dino.korah@redmatter.com>
2016-07-27 14:00:45 +0100

    PLAT-6874 Improvements to hostname guessing
    Also updated the docs
    
    +review C-PLAT-1442

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,22 @@ set -eu
 COLLECTD_CONF=/etc/collectd/collectd.conf
 
 : ${COLLECTD_HOST:=}
-if [ -z "${COLLECTD_HOST}" -a -e "/host/hostname" ]; then
-    COLLECTD_HOST=$(cat /host/hostname);
+# if COLLECTD_HOST not already set in the container's environment
+if [ -z "${COLLECTD_HOST}" ]; then
+    # if /etc/hostname is volume mounted in, then use that
+    if [ -f "/host/hostname" ]; then
+        COLLECTD_HOST=$(cat /host/hostname);
+    # else if /proc/sys/kernel/hostname is volume mounted in, then use that
+    elif [ -f "/host/proc/sys/kernel/hostname" ]; then
+        COLLECTD_HOST=$(cat /host/proc/sys/kernel/hostname);
+    fi
 fi
+# after all that, if COLLECTD_HOST is finally set, then sed up the config
 if [ -n "$COLLECTD_HOST" ]; then
     sed -i -e "s/# @COLLECTD_HOST@/Hostname ${COLLECTD_HOST//-/}/g" $COLLECTD_CONF
 fi
 
+# default collectd interval to 10s; overriden using COLLECTD_INTERVAL_SECONDS
 : ${COLLECTD_INTERVAL_SECONDS:=10}
 sed -i -e "s/# @COLLECTD_INTERVAL_SECONDS@/Interval $COLLECTD_INTERVAL_SECONDS/g" $COLLECTD_CONF
 
@@ -22,8 +31,10 @@ if [ ! -d /mnt/oldproc -a -d /host/proc ]; then
     mount -t proc none /mnt/oldproc
 fi
 
+# if no command specified, start collectd
 if [ -z "$@" ]; then
     exec /usr/sbin/collectd -C $COLLECTD_CONF -f
 fi
 
+# otherwise run the command
 exec "$@"
